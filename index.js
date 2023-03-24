916fb51e7ac9ce065248d7df8c40999fbe8b957e
martscastrillo <mart.castrillo@gmail.com>
2023-03-24 09:34:43 +0100

    add first steps

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let btnRef = document.querySelectorAll(".button_option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new_game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
